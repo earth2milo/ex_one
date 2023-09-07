31737bd363d7cc00ba447393379e96ba885c17fb
earth2milo <90530636+earth2milo@users.noreply.github.com>
2023-09-07 09:45:26 -0400

    in-class code flexbox and box model

---
+++ Untitled-1.js
@@ -0,0 +1,23 @@
+// Variables 
+// var nameofVariable = ''
+
+const constantVariable = 'VALUE';
+// Const is static 
+let changableVariable = 'VALUE';
+console.log(changableVariable);
+// Let as a function can change 
+
+// * Arrays //
+const coolrray = [
+    'string',
+    0,
+{},
+    0.4
+];
+
+console.log(coolArray[4]);
+coolArray[0] = 'another string';
+
+// Numbers/Math 
+
+
