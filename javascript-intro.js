1ca7c11beea31924d5813d9b1a7f6ecd245d1f04
earth2milo <90530636+earth2milo@users.noreply.github.com>
2023-09-12 09:28:54 -0400

    positioning, header, and js

---
+++ javascript-intro.js
@@ -0,0 +1,44 @@
+// # Javascript 
+
+
+## Variables
+let
+const
+## Arrays
+// [1,2, 'hi']
+## Functions
+function coolFunction(arg) {
+    return arg;
+}
+
+const coolArrowFunction = () => 'hello';
+
+
+## Objects
+
+// {
+//     "key": "value"
+// }
+
+// {
+//     top: [
+//         {
+//             middle: {
+//                 bottom: {
+//                     thing: 'frogs'
+//                         }
+//             }
+//         }
+    
+//        ]
+// }
+
+annoyingObject.top[0] middle bottom thing // 'frogs'
+
+
+//const JSONExample = [
+//   { "key"; "value" 
+//        }
+//];
+
+//
